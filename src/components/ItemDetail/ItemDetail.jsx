2de0b5b57dcc2e64e38e6133e7c812950d8c9c4c
Florencia Montenegro <mflortrabajos@gmail.com>
2022-06-27 19:37:08 -0300

    cambio de a por Link

--- src/components/ItemDetail/ItemDetail.jsx
+++ src/components/ItemDetail/ItemDetail.jsx
@@ -1,17 +1,17 @@
 import React from "react";
 import ItemCount from '../ItemCount/ItemCount'
 
-const itemDetail = ({id, price, title, pictureUrl, category}) => {
+const itemDetail = ({id, price, title, pictureUrl, category, productDetail}) => {
   return (
     <div className={"ItemListContainer flex grid-cols-3 w-full sm:w-1/2 md:w-1/2 lg:w-1/3 xl:w-1/3 2xl:w-1/4 p-4 flex-none"}>  
         <div className={'m-5  max-w-sm bg-grey rounded-lg border border-gray-200 shadow-md dark:border-gray-900 bg-stone-400/20'}> 
         <img src={pictureUrl} alt=""/> 
         <div className={"p-5"}> 
         <a href="perfumes.html">
-          <h5 className={"mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-black"}>{title} - {id}</h5> </a> 
+          <h5 className={"mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-black"}>{title} - {id}- {category}</h5> </a> 
         <p className={"mb-3 font-mono text-white dark:text-black"}>${price}</p> 
         </div>  
-        <ItemCount stock={5} initial={1}/>
+        <ItemCount stock={5} initial={1} productDetail={productDetail}/>
         </div> 
     </div> 
 )}
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,13 +1,14 @@
 import Logo from '../img/logo_large.png'
 import CartWidget from '../CartWidget/CartWidget';
+import { Link } from 'react-router-dom';
 
 const NavBar = () => {
     return (
 <nav className="bg-zinc-100 border-gray-200 px-2 sm:px-4 py-2.5 rounded ">
   <div className="container flex flex-wrap justify-between items-center mx-auto">
-    <a href="index.html" className="flex items-center">
+    <Link to='/' className="flex items-center">
         <img src={Logo} className="mr-3 h-6 sm:h-11" alt="PerfuStore Logo" />
-    </a>
+        </Link>
     <button data-collapse-toggle="mobile-menu" type="button" className="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="mobile-menu" aria-expanded="false">
       <span className="sr-only">Open main menu</span>
 
@@ -20,13 +21,13 @@ const NavBar = () => {
     <div className="hidden w-full md:block md:w-auto" id="mobile-menu">
       <ul className="flex flex-col mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-xl md:font-medium">
       <li>
-          <a href="index.html" className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-black md:dark:hover:text-gray-700 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">HOME</a>
+          <Link to='/' className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-black md:dark:hover:text-gray-700 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">HOME</Link>
         </li>
         <li>
-          <a href="index.html" className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-black md:dark:hover:text-gray-700 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">PERFUMES</a>
+          <a to='/' href='/' className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-black md:dark:hover:text-gray-700 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">PERFUMES</a>
         </li>
         <li>
-          <a href="index.html" className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-black md:dark:hover:text-gray-700 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">CONTACTO</a>
+          <a to='/' href='/' className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-black md:dark:hover:text-gray-700 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">CONTACTO</a>
         </li>
         <li><a href="carrito.html"><CartWidget /></a></li>
       </ul>
