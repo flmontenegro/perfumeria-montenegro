549d1c9861aa80f01ddf3251db35140b46253d0a
Florencia Montenegro <mflortrabajos@gmail.com>
2022-07-05 19:38:46 -0300

    correcion counter

--- src/components/ItemCount/ItemCount.jsx
+++ src/components/ItemCount/ItemCount.jsx
@@ -1,5 +1,5 @@
 import { React, useState } from 'react'
-import { Link } from "react-router-dom";
+
 
 function ItemCount ({ stock, initial, onAdd}) {
     const [counter, setCounter] = useState(initial)
@@ -28,8 +28,6 @@ return (
     <button type="button" onClick={() => onAdd(counter)} className="text-white  inline-flex items-center bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center mr-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"><svg class="w-5 h-5 mr-2 -ml-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M3 1a1 1 0 000 2h1.22l.305 1.222a.997.997 0 00.01.042l1.358 5.43-.893.892C3.74 11.846 4.632 14 6.414 14H15a1 1 0 000-2H6.414l1-1H14a1 1 0 00.894-.553l3-6A1 1 0 0017 3H6.28l-.31-1.243A1 1 0 005 1H3zM16 16.5a1.5 1.5 0 11-3 0 1.5 1.5 0 013 0zM6.5 18a1.5 1.5 0 100-3 1.5 1.5 0 000 3z"></path></svg> CANTIDAD
 </button>
 </div>
-<Link to={`/cart`}><button type="button" className="text-white mb-3 bg-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center mr-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"><path d="M3 1a1 1 0 000 2h1.22l.305 1.222a.997.997 0 00.01.042l1.358 5.43-.893.892C3.74 11.846 4.632 14 6.414 14H15a1 1 0 000-2H6.414l1-1H14a1 1 0 00.894-.553l3-6A1 1 0 0017 3H6.28l-.31-1.243A1 1 0 005 1H3zM16 16.5a1.5 1.5 0 11-3 0 1.5 1.5 0 013 0zM6.5 18a1.5 1.5 0 100-3 1.5 1.5 0 000 3z"></path> FINALIZAR COMPRA
-</button></Link>
 </div>
 )
 }
--- src/components/ItemDetail/ItemDetail.jsx
+++ src/components/ItemDetail/ItemDetail.jsx
@@ -1,12 +1,19 @@
-import React from "react";
+import { React , useState } from "react";
 import ItemCount from '../ItemCount/ItemCount'
 import './ItemDetail.css'
+import { Link } from "react-router-dom";
 
-const itemDetail = ({ price, title,marca, pictureUrl, productDetail, description, presentacion}) => {
-  function onAdd(counter) {
-    alert("Cantidad de productos agregados: "+ counter);
+const ItemDetail = ({ price, title,marca, pictureUrl, productDetail, description, presentacion}) => {
+  
+  const [auxCount, setAuxCount] = useState();
+  
+  function onAdd(countParam) {
+    setAuxCount (countParam);
+    alert("Ha agregado "+ countParam + " " + title+ " al carrito de compras." );
+    console.log('setAuxCount'+ auxCount)
     }
-  return (
+  
+    return (
     <div className={" flex place-content-center  center "}>  
         <div className={'m-5  max-w-sm bg-grey rounded-lg border border-gray-200 shadow-md dark:border-gray-900 bg-stone-400/20'}> 
         <img src={pictureUrl} alt=""/> 
@@ -21,9 +28,11 @@ const itemDetail = ({ price, title,marca, pictureUrl, productDetail, description
         <p className={"text-2xl mb-5 font-bold text-white dark:text-black"}>${price}</p> 
         </div>  
         <ItemCount stock={5} initial={1} onAdd={onAdd} productDetail={productDetail}/>
+        <Link to={`/cart`}><button type="button" className="text-white mb-3 bg-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center mr-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"><path d="M3 1a1 1 0 000 2h1.22l.305 1.222a.997.997 0 00.01.042l1.358 5.43-.893.892C3.74 11.846 4.632 14 6.414 14H15a1 1 0 000-2H6.414l1-1H14a1 1 0 00.894-.553l3-6A1 1 0 0017 3H6.28l-.31-1.243A1 1 0 005 1H3zM16 16.5a1.5 1.5 0 11-3 0 1.5 1.5 0 013 0zM6.5 18a1.5 1.5 0 100-3 1.5 1.5 0 000 3z"></path> FINALIZAR COMPRA
+</button></Link>
         </div> 
     </div> 
 )}
 
 
-export default itemDetail
+export default ItemDetail
