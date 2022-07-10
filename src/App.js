d67543c4573a2fceb89952cfe1523d2ca52ab139
Florencia Montenegro <mflortrabajos@gmail.com>
2022-07-10 11:49:45 -0300

    context no resuelto, maquetado de cart

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import ItemListContainer from './components/ItemListContainer/ItemListContainer'
 import ItemDetailContainer from './components/ItemDetail/ItemDetailContainer';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import CartContext from './Context/CartContext';
+import Cart from './components/Cart/Cart';
 
 
 function App() {
@@ -17,6 +18,7 @@ function App() {
             <Route path="/" element={<ItemListContainer/>} />
             <Route path="/category/:categoryId" element={<ItemListContainer/>} />
             <Route path="/item/:itemId" element={<ItemDetailContainer/>} />
+            <Route path="/Cart" element={<Cart/>} />
         </Routes>
       </CartContext>
       </BrowserRouter>
--- src/components/ItemDetail/ItemDetail.jsx
+++ src/components/ItemDetail/ItemDetail.jsx
@@ -32,7 +32,7 @@ const ItemDetail = ({ price, title,marca, pictureUrl, productDetail,productList,
         <p className={"text-2xl mb-5 font-bold text-white dark:text-black"}>${price}</p> 
         </div>  
         <ItemCount stock={5} initial={1} onAdd={onAdd} productDetail={productDetail}/>
-        <Link to={`/cart`}><button onClick={()=> addItem(productList,auxCount)} type="button" className="text-white mb-3 bg-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center mr-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"><path d="M3 1a1 1 0 000 2h1.22l.305 1.222a.997.997 0 00.01.042l1.358 5.43-.893.892C3.74 11.846 4.632 14 6.414 14H15a1 1 0 000-2H6.414l1-1H14a1 1 0 00.894-.553l3-6A1 1 0 0017 3H6.28l-.31-1.243A1 1 0 005 1H3zM16 16.5a1.5 1.5 0 11-3 0 1.5 1.5 0 013 0zM6.5 18a1.5 1.5 0 100-3 1.5 1.5 0 000 3z"></path> FINALIZAR COMPRA
+        <Link to={`/Cart`}><button onClick={()=> addItem(productList,auxCount)} type="button" className="text-white mb-3 bg-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center mr-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"><path d="M3 1a1 1 0 000 2h1.22l.305 1.222a.997.997 0 00.01.042l1.358 5.43-.893.892C3.74 11.846 4.632 14 6.414 14H15a1 1 0 000-2H6.414l1-1H14a1 1 0 00.894-.553l3-6A1 1 0 0017 3H6.28l-.31-1.243A1 1 0 005 1H3zM16 16.5a1.5 1.5 0 11-3 0 1.5 1.5 0 013 0zM6.5 18a1.5 1.5 0 100-3 1.5 1.5 0 000 3z"></path> FINALIZAR COMPRA
 </button></Link>
         </div> 
     </div> 
