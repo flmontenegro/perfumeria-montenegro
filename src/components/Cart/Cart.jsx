eb301bc20965cbddb6487b04a979b0136f4d6e83
Florencia Montenegro <mflortrabajos@gmail.com>
2022-07-24 20:34:48 -0300

    desafio Item Collection

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -3,9 +3,10 @@ import { useContext } from 'react';
 import { CartContext } from '../../Context/CartContext';
 import { Link } from 'react-router-dom';
 
+
 export default function Cart() {
     const { cart, removeItem, totalProduct} = useContext(CartContext);
-
+    
     return (
         cart.length === 0 ? (
             <> <h1 className={'text-[50px] font-semibold text-zinc-500 font-serif'}>SU CARRO ESTA VACIO</h1>
@@ -13,44 +14,44 @@ export default function Cart() {
 </Link>
             </> ): (
         <>
-            <div>
-        <div className=" place-items-center flex flex-col max-w-3xl p-6 space-y-4 sm:p-10 dark:bg-gray-900 dark:text-gray-100">
-        <h2 className="text-xl font-semibold">DETALLE DE COMPRA</h2>
-                { cart.map((row) => (
-                    <div  className="flex flex-col max-w-3xl p-6 space-y-4 sm:p-10 dark:bg-gray-900 dark:text-gray-100">
-                    
-                    <ul className="flex flex-col divide-y divide-gray-700">
-                        <li className="flex flex-col py-6 sm:flex-row sm:justify-between">
-                            <div className="flex w-full space-x-2 sm:space-x-4">
-                                <img className="flex-shrink-0 object-cover w-20 h-20 dark:border-transparent rounded outline-none sm:w-32 sm:h-32 dark:bg-gray-500" alt="img" src={row.pictureUrl} />
-                                <div  className="flex flex-col justify-between w-full pb-4">
-                                    <div className="flex justify-between w-full pb-2 space-x-2">
-                                        <div   className="space-y-1" >
-                                        
-                                            <h3 key={row.id} className="text-lg font-semibold leading-snug sm:pr-8" >{row.title}</h3>
-                                        </div>
-                                        <div className="text-right">
-                                            <p className="text-lg font-semibold"> CANTIDAD: {row.cantidad}</p>
-                                            
-                                        </div>
-                                    </div>
-                                    <div className="flex text-sm divide-x">
-                                        <button onClick={() => removeItem(row.id)} type="button" className="flex items-center px-2 py-1 pl-0 space-x-1">
-                                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" className="w-4 h-4 fill-current">
-                                                <path d="M96,472a23.82,23.82,0,0,0,23.579,24H392.421A23.82,23.82,0,0,0,416,472V152H96Zm32-288H384V464H128Z"></path>
-                                                <rect width="32" height="200" x="168" y="216"></rect>
-                                                <rect width="32" height="200" x="240" y="216"></rect>
-                                                <rect width="32" height="200" x="312" y="216"></rect>
-                                                <path d="M328,88V40c0-13.458-9.488-24-21.6-24H205.6C193.488,16,184,26.542,184,40V88H64v32H448V88ZM216,48h80V88H216Z"></path>
-                                            </svg>
-                                            <span>Remove</span>
-                                        </button>
-                                    </div>
-                                </div>
-                            </div>
-                        </li>
+    <div>
+    <div className=" flex flex-col max-w-3xl p-6 space-y-4 sm:p-10 dark:bg-gray-900 dark:text-gray-100">
+    <h2 className="text-xl font-semibold">DETALLE DE COMPRA</h2>
+        { cart.map((row) => (
+        <div  className="flex flex-col max-w-3xl p-6 space-y-4 sm:p-10 dark:bg-gray-900 dark:text-gray-100">
+            <ul className="flex flex-col divide-y divide-gray-700">
+            <li className="flex flex-col py-6 sm:flex-row sm:justify-between">
+                 <div className="flex w-full space-x-2 sm:space-x-4">
+                <img className="flex-shrink-0 object-cover w-20 h-20 dark:border-transparent rounded outline-none sm:w-32 sm:h-32 dark:bg-gray-500" alt="img" src={row.pictureUrl} />
+                 <div  className="flex flex-col justify-between w-full pb-4">
+                    <div className="flex justify-between w-full pb-2 space-x-2">
+                    <div   className="space-y-1" >
+                    <h3 key={row.id} className="text-lg font-semibold leading-snug sm:pr-8" >{row.title}</h3>
+                    <p>{row.marca}</p>
+                    </div>
+                    <div><label for="countries" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400">CANTIDAD</label>
+<div id="countries" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+{row.cantidad}
+</div></div>
+                    <div className="flex text-sm divide-x">
+                    <button onClick={() => removeItem(row.id)} type="button" className="flex items-center px-2 py-1 pl-0 space-x-1">
+                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" className="w-4 h-4 fill-current">
+                         <path d="M96,472a23.82,23.82,0,0,0,23.579,24H392.421A23.82,23.82,0,0,0,416,472V152H96Zm32-288H384V464H128Z"></path>
+                         <rect width="32" height="200" x="168" y="216"></rect>
+                        <rect width="32" height="200" x="240" y="216"></rect>
+                        <rect width="32" height="200" x="312" y="216"></rect>
+                        <path d="M328,88V40c0-13.458-9.488-24-21.6-24H205.6C193.488,16,184,26.542,184,40V88H64v32H448V88ZM216,48h80V88H216Z"></path>
+                        </svg>
+                        <span>Remove</span>
+                    </button>
+                    </div>
+                    </div>
+                    </div>
+                    </div></li>
                     </ul>
+                    
                     <div className="space-y-1 text-right">
+                        <p className="text-lg font-semibold"> CANTIDAD: {row.cantidad}</p>
                         <p>SUBTOTAL: 
                             <span className="font-semidbol">$ {row.subtotal}</span>
                         </p>
--- src/components/ItemDetail/ItemDetailContainer.jsx
+++ src/components/ItemDetail/ItemDetailContainer.jsx
@@ -8,32 +8,34 @@ const ItemDetailContainer =()=> {
     const [productDetail, setProductDetail] = useState({})
     const {itemId} = useParams();
 
-    useEffect(() => { 
-      let products = [ 
-        {id: 1 , title: "212 HEROES", price: 10000,marca:'Carolina Herrera', category:'CarolinaHerrera', presentacion:'80ml', description:"Notas predominantes: mandarina, frambuesa, flores de jazmin, naranja, cedró y sándalo", pictureUrl: 'https://juleriaque.vteximg.com.br/arquivos/ids/184123-1000-1000/212-heroes-for-her-edp-C054449D85291FEC34B3773068DD484A.jpg?v=637834755297430000'}, 
-        {id: 2 , title: "MISS DIOR", price: 13000,marca:'Dior',category: 'Dior', presentacion:'50ml', description: "Notas de Corazón: Rosa Centifolia. Notas de Fondo: Maderas tiernas.Notas de Salida: Las notas de un muguete con floralidad fresca, el acorde de peonía expresiva y las notas de suave iris acompañan un bello verdor ligeramente humedecido.Aroma: Floral." , pictureUrl: 'https://juleriaque.vteximg.com.br/arquivos/ids/177794-1000-1000/miss-dior-edp-2BC2BFEDC5A1BB8C09A6490E464212FC.jpg?v=637678261843170000'}, 
-        {id: 3 , title: "XS PURE", price: 15000,marca:'Paco Rabanne',category:'PacoRabanne',presentacion:'80ml',description:"Notas de entrada: flor de tamarindo, frutos silvestres. Notas de corazón: rosa eléboro. Notas de salida: vainilla negra. Aroma: Floral, amaderada, afrutada.", pictureUrl: 'https://juleriaque.vteximg.com.br/arquivos/ids/166535-1000-1000/pure-xs-for-her-edp-71B78C8FEBEC27223C2B23F5702FAB7D.jpg?v=637347517235800000' },
-        {id: 4 , title: "J'ADORE", price: 18000 ,marca:'Dior',category:'Dior',presentacion:'100ml', description:"Un aroma floral luminoso, alegre, asentado y decidido. Armoniza flores como la Champaca (variedad de magnolia de la India, de olor fresco, afrutado y exótico), la violeta de Francia y la orquídea cristalizada. Como fondo, la madera de amaranto, cuyo aroma recuerda al del clavel con más madera y suavidad; se armoniza con una nota de vino dulce de Banyuls y ciruela confitada.", pictureUrl: 'https://juleriaque.vteximg.com.br/arquivos/ids/170565-1000-1000/jadore-infinissime-edp--976BFAF0A599FE2361B3395E47D64863.jpg?v=637455397900830000' },
-        {id: 5 , title: "SI- ARMANI", price: 15700 ,marca:'Armani', category:'Armani',presentacion:'100ml', description:"Familia olfativa: Chipre Frutal. Notas de salida: licor de cassis. Notas de corazón: fresia, rosa de mayo, pachulí. Notas de fondo: musgo, maderas claras.",pictureUrl: 'https://juleriaque.vteximg.com.br/arquivos/ids/157664-1000-1000/si-edp.jpg?v=637336348274270000' },
-        {id: 6 , title: "CH EDT", price: 19000 ,marca:'Carolina Herrera',category:'CarolinaHerrera',presentacion:'80ml',description:"Notas de entrada: Bergamota, Uva. Notas de corazón: Rosa búlgara, Jazmín Sambac. Notas de salida: Pachuli, notas de almendra garrapiñada. Aroma: Floral.",pictureUrl: 'https://juleriaque.vteximg.com.br/arquivos/ids/164491-1000-1000/ch-edt-158CA0E7837E77AC7C1AD0C53C7F3AC1.jpg?v=637347500595000000' } ]; 
+     useEffect(() => { 
+    //   let products = [ 
+    //     {id: 1 , title: "212 HEROES", price: 10000,marca:'Carolina Herrera', category:'CarolinaHerrera', presentacion:'80ml', description:"Notas predominantes: mandarina, frambuesa, flores de jazmin, naranja, cedró y sándalo", pictureUrl: 'https://juleriaque.vteximg.com.br/arquivos/ids/184123-1000-1000/212-heroes-for-her-edp-C054449D85291FEC34B3773068DD484A.jpg?v=637834755297430000'}, 
+    //     {id: 2 , title: "MISS DIOR", price: 13000,marca:'Dior',category: 'Dior', presentacion:'50ml', description: "Notas de Corazón: Rosa Centifolia. Notas de Fondo: Maderas tiernas.Notas de Salida: Las notas de un muguete con floralidad fresca, el acorde de peonía expresiva y las notas de suave iris acompañan un bello verdor ligeramente humedecido.Aroma: Floral." , pictureUrl: 'https://juleriaque.vteximg.com.br/arquivos/ids/177794-1000-1000/miss-dior-edp-2BC2BFEDC5A1BB8C09A6490E464212FC.jpg?v=637678261843170000'}, 
+    //     {id: 3 , title: "XS PURE", price: 15000,marca:'Paco Rabanne',category:'PacoRabanne',presentacion:'80ml',description:"Notas de entrada: flor de tamarindo, frutos silvestres. Notas de corazón: rosa eléboro. Notas de salida: vainilla negra. Aroma: Floral, amaderada, afrutada.", pictureUrl: 'https://juleriaque.vteximg.com.br/arquivos/ids/166535-1000-1000/pure-xs-for-her-edp-71B78C8FEBEC27223C2B23F5702FAB7D.jpg?v=637347517235800000' },
+    //     {id: 4 , title: "J'ADORE", price: 18000 ,marca:'Dior',category:'Dior',presentacion:'100ml', description:"Un aroma floral luminoso, alegre, asentado y decidido. Armoniza flores como la Champaca (variedad de magnolia de la India, de olor fresco, afrutado y exótico), la violeta de Francia y la orquídea cristalizada. Como fondo, la madera de amaranto, cuyo aroma recuerda al del clavel con más madera y suavidad; se armoniza con una nota de vino dulce de Banyuls y ciruela confitada.", pictureUrl: 'https://juleriaque.vteximg.com.br/arquivos/ids/170565-1000-1000/jadore-infinissime-edp--976BFAF0A599FE2361B3395E47D64863.jpg?v=637455397900830000' },
+    //     {id: 5 , title: "SI- ARMANI", price: 15700 ,marca:'Armani', category:'Armani',presentacion:'100ml', description:"Familia olfativa: Chipre Frutal. Notas de salida: licor de cassis. Notas de corazón: fresia, rosa de mayo, pachulí. Notas de fondo: musgo, maderas claras.",pictureUrl: 'https://juleriaque.vteximg.com.br/arquivos/ids/157664-1000-1000/si-edp.jpg?v=637336348274270000' },
+    //     {id: 6 , title: "CH EDT", price: 19000 ,marca:'Carolina Herrera',category:'CarolinaHerrera',presentacion:'80ml',description:"Notas de entrada: Bergamota, Uva. Notas de corazón: Rosa búlgara, Jazmín Sambac. Notas de salida: Pachuli, notas de almendra garrapiñada. Aroma: Floral.",pictureUrl: 'https://juleriaque.vteximg.com.br/arquivos/ids/164491-1000-1000/ch-edt-158CA0E7837E77AC7C1AD0C53C7F3AC1.jpg?v=637347500595000000' } ]; 
 
-        new Promise((resolve, reject) => {
-            setTimeout(()=>{
-                resolve(products)
-            },2000 )
-        }).then(res=> {
-            const item = res.find((products)=> products.id === Number(itemId))
-            setProductDetail(item)
-        })
+    //     new Promise((resolve, reject) => {
+    //         setTimeout(()=>{
+    //             resolve(products)
+    //         },2000 )
+    //     }).then(res=> {
+    //         const item = res.find((products)=> products.id === Number(itemId))
+    //         setProductDetail(item)
+    //     })
 
-        const db = getFirestore();
-       const docRef = doc(db,'productos',itemId);
-       getDoc(docRef).then((item)=>{
+    const db = getFirestore();
 
-        const aux = {...item.data(), id:item.id};
-        setProductDetail(aux);
-        })
-    }, [itemId])
+    const docRef = doc(db,'productos',itemId);
+    getDoc(docRef).then((item)=>{
+  
+      const aux = {...item.data(), id:item.id};
+      setProductDetail(aux);
+      })
+  
+  }, [itemId])
   
     return ( <ItemDetail {...productDetail} />
   )
